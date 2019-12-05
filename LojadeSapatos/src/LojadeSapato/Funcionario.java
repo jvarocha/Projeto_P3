11362c1eedabd961d87b2e0e83f59c767c4bd75e
jvarocha <32077403+jvarocha@users.noreply.github.com>
2019-12-04 23:35:39 -0300

    Update Funcionario.java

--- LojadeSapatos/src/LojadeSapato/Funcionario.java
+++ LojadeSapatos/src/LojadeSapato/Funcionario.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Funcionario {
 	Scanner teclado = new Scanner(System.in);
 	private String nome;
+	private int cpf;
 	private String login;
 	private String senha;
 	public Funcionario() {
@@ -14,6 +15,16 @@ public class Funcionario {
 		this.login = teclado.nextLine();
 		System.out.println("Senha:");
 		this.senha = teclado.nextLine();
+		System.out.println("CPF:");
+		this.cpf = teclado.nextInt();
+	}
+	public void verificarletras(String letras) {
+		for (int i = 0; i < letras.length(); i++) {
+	          if (Character.isDigit(letras.charAt(i))==true){
+	              System.out.println("Entrada Inválida! Possui número(s) em nome");
+	              nome = "-1";
+	          }
+	        }
 	}
 	public String getLogin() {
 		return login;
@@ -34,4 +45,11 @@ public class Funcionario {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
 }
