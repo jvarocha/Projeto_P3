8e5a7cdd0de4a66044959d279b37d7c6cfb8285c
jvarocha <32077403+jvarocha@users.noreply.github.com>
2019-12-04 23:34:17 -0300

    Update Gerente.java

--- LojadeSapatos/src/LojadeSapato/Gerente.java
+++ LojadeSapatos/src/LojadeSapato/Gerente.java
@@ -1,5 +1,53 @@
 package LojadeSapato;
 
-public class Gerente {
+import java.util.Scanner;
 
+public class Gerente {
+	Scanner teclado = new Scanner(System.in);
+	private String nome;
+	private int cpf;
+	private String login;
+	private String senha;
+	public Gerente() {
+		System.out.println("Nome:");
+		this.nome = teclado.nextLine();
+		System.out.println("Login:");
+		this.login = teclado.nextLine();
+		System.out.println("Senha:");
+		this.senha = teclado.nextLine();
+		System.out.println("CPF:");
+		this.cpf = teclado.nextInt();
+	}
+	public void verificarletras(String letras) {
+		for (int i = 0; i < letras.length(); i++) {
+	          if (Character.isDigit(letras.charAt(i))==true){
+	              System.out.println("Entrada Inválida! Possui número(s) em nome");
+	              nome = "-1";
+	          }
+	        }
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
